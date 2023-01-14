b616c7136ad6e8139faf0fef53b9d68a566ff606
Phanor Coll <phanorcoll@gmail.com>
2023-01-14 11:46:11 -0400

    ✏️ Fixed type on Injectable
    fixed type when assinging Injectable().
    
    ✏️ related to #2

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -3,7 +3,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Injectable
+@Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
     super(); //passes the strategy options to PassportStrategy

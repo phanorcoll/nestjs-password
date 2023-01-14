93bebb5d205ae40b1762b46644c98783d3302907
Phanor Coll <phanorcoll@gmail.com>
2023-01-14 13:35:37 -0400

    🚨 Linter Warnings
    Fixed some linters warnings.
    
    ✏️ no task related.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,10 +15,10 @@ import { JwtStrategy } from './jwt.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s'},
-    })
+      signOptions: { expiresIn: '60s' },
+    }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
-  exports: [AuthService]
+  exports: [AuthService],
 })
 export class AuthModule {}

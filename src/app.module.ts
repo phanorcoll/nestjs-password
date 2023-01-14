2c3b8666dd176132b4d6768985e443a1df918f80
Phanor Coll <phanorcoll@gmail.com>
2023-01-14 16:59:53 -0400

    🗃️ Added Mongodb
    installed npm packages and configured project to use Monbodb,
    also created the docker compose file to start a mongo container.
    
    ✏️ close #5

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule} from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    MongooseModule.forRoot('mongodb://root:toor@0.0.0.0:27017', {
+      dbName: 'nestjs',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
